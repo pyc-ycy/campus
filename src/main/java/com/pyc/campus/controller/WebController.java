07433d7c4d4345f8c77d88f6558497fede9f74ae
7wc98#14 <2923616405@qq.com>
2021-01-01 12:52:47 +0800

    modify

--- src/main/java/com/pyc/campus/controller/WebController.java
+++ src/main/java/com/pyc/campus/controller/WebController.java
@@ -533,6 +533,44 @@ public class WebController {
             return "page/UpQuestion";
         }
     }
+    @RequestMapping("feedback")
+    public String feedback(Model model, HttpSession session,
+                           @Param("mail")String mail, @Param("name")String name,
+                           @Param("title")String title, @Param("content")String content){
+        SecurityContextImpl securityContext = (SecurityContextImpl)session.getAttribute("SPRING_SECURITY_CONTEXT");
+        String currentStudentId = ((UserDetails) securityContext.getAuthentication().getPrincipal()).getUsername();
+        Student s = studentRepository.findNameByStudentID(currentStudentId);
+        model.addAttribute("curUse",s);
+        try{
+            MailConfig mailConfig = new MailConfig();
+            JavaMailSender sender = mailConfig.getMailSender();
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom("15014366986@163.com");
+            mailMessage.setTo("2923616405@qq.com");
+            mailMessage.setSubject("用户反馈");
+            String text = "反馈用户："+name+"，邮箱："+mail+",问题标题："+title+",问题内容："+content;
+            mailMessage.setText(text);
+            sender.send(mailMessage);
+            Msg msg = new Msg("提示","反馈成功，请等待管理员回复","");
+            model.addAttribute("msg",msg);
+            return "page/FeedBack";
+        }catch (Exception e){
+            Msg msg = new Msg("提示","发生错误，请重新尝试，谢谢","");
+            model.addAttribute("msg",msg);
+            System.out.println(e.getMessage());
+            return "page/FeedBack";
+        }
+    }
+    @RequestMapping("/toFeedBack")
+    public String toFeedBack(Model model, HttpSession session){
+        SecurityContextImpl securityContext = (SecurityContextImpl)session.getAttribute("SPRING_SECURITY_CONTEXT");
+        String currentStudentId = ((UserDetails) securityContext.getAuthentication().getPrincipal()).getUsername();
+        Student s = studentRepository.findNameByStudentID(currentStudentId);
+        model.addAttribute("curUse",s);
+        Msg msg = new Msg("","","");
+        model.addAttribute("msg",msg);
+        return "page/FeedBack";
+    }
     @RequestMapping("/toPublishQuestion")
     public String toPublishQuestion(Model model,HttpSession session){
         SecurityContextImpl securityContext = (SecurityContextImpl)session.getAttribute("SPRING_SECURITY_CONTEXT");
